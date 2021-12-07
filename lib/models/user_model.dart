20e7087301f79279963ebc2fed4f7d2e8a35308c
Numb4r <yuridimitre@protonmail.com>
2021-12-06 22:11:25 -0300

    modificado user model para refletir o bd

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,16 +1,43 @@
+//TODO: Retirar possibilidade de null assigment
+// null assigment feito apenas para testes
 class User {
-  final String name;
-  final String email;
+  late String name;
+  late String email;
+  late String? cpf;
+  late String? birthDay;
+  late String? userType;
+  late String? isAdmin;
+  late String? password;
 
-  User({
-    required this.name,
-    required this.email,
-  });
+  User(
+      {required this.name,
+      required this.email,
+      this.cpf,
+      this.birthDay,
+      this.userType,
+      this.isAdmin,
+      this.password});
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-      name: json['name'],
-      email: json['email'],
+      cpf: json["cpf"],
+      name: json["name"],
+      birthDay: json["birthDay"],
+      userType: json["userType"],
+      email: json["email"],
+      isAdmin: json["isAdmin"],
+      password: json["password"],
     );
+  }
+  toMap() {
+    return <String, dynamic>{
+      "cpf": cpf,
+      "name": name,
+      "birthDay": birthDay,
+      "userType": userType,
+      "email": email,
+      "isAdmin": isAdmin,
+      "password": password,
+    };
   }
 }
