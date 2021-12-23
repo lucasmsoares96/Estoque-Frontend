fd0c687c86ba8984ffdcd34dee5a67806e773d16
Sávio rodrigues <saviorodrigues012@gmail.com>
2021-12-23 07:59:49 -0300

    45 - Janela de adição e alteração de produto

---
+++ lib/pages/linkPages/gerenciador/add_product_popup.dart
@@ -0,0 +1,244 @@
+import 'package:flutter/material.dart';
+
+class AddProduct extends StatefulWidget {
+  const AddProduct({Key? key}) : super(key: key);
+
+  @override
+  _AddProductState createState() => _AddProductState();
+}
+
+class _AddProductState extends State<AddProduct> {
+  inputDecoration(String hint) {
+    return InputDecoration(
+      focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.black),
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+      border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          borderSide: BorderSide(color: Colors.black)),
+      hintText: hint,
+    );
+  }
+
+  final TextEditingController _nameProduct = TextEditingController();
+  final TextEditingController _tagProduct = TextEditingController();
+  final TextEditingController _quantProduct = TextEditingController(text: "0");
+  int _quantidade = 0;
+  double _maxHeight = 375;
+  final _formKey = GlobalKey<FormState>();
+
+  _addQuantidade() {
+    setState(
+      () {
+        _quantidade = (int.tryParse(_quantProduct.text)!) + 1;
+        _quantProduct.text = _quantidade.toString();
+      },
+    );
+  }
+
+  _subQuantidade() {
+    setState(
+      () {
+        _quantidade = (int.tryParse(_quantProduct.text)!) - 1;
+        _quantProduct.text = _quantidade.toString();
+      },
+    );
+  }
+
+  _updateHeight() {
+    if (_maxHeight < 415) {
+      setState(
+        () {
+          _maxHeight = _maxHeight + 60;
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Container(
+        constraints: BoxConstraints(
+          minWidth: 400,
+          maxWidth: 500,
+          minHeight: 375,
+          maxHeight: _maxHeight,
+        ),
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(20))),
+        width: MediaQuery.of(context).size.width,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Adicionar produto",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 5),
+                child: Text(
+                  "Nome :",
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+              TextFormField(
+                cursorColor: Colors.black,
+                decoration: inputDecoration("Digite o nome do produto"),
+                controller: _nameProduct,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Preencha o nome do produto";
+                  }
+                  return null;
+                },
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 5),
+                child: Text(
+                  "Tag :",
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+              TextFormField(
+                cursorColor: Colors.black,
+                decoration: inputDecoration("Digite um tag para o produto"),
+                controller: _tagProduct,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Preencha um tag para o produto";
+                  }
+                  return null;
+                },
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 5),
+                child: Text(
+                  "Quantidade :",
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+              SizedBox(
+                height: 55,
+                child: Row(
+                  children: [
+                    Container(
+                      constraints:
+                          const BoxConstraints(minHeight: 40, maxHeight: 45),
+                      width: 140,
+                      child: TextFormField(
+                        readOnly: true,
+                        textAlign: TextAlign.center,
+                        textAlignVertical: TextAlignVertical.bottom,
+                        controller: _quantProduct,
+                        decoration: inputDecoration(''),
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value == "0") {
+                            return "Quantidade inválida.";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Container(
+                        height: 25,
+                        width: 25,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(color: Colors.grey.shade700)),
+                        child: IconButton(
+                          padding: const EdgeInsets.all(0),
+                          splashRadius: 15,
+                          onPressed: _addQuantidade,
+                          icon: Icon(
+                            Icons.add,
+                            color: Colors.grey.shade700,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 25,
+                      width: 25,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          border: Border.all(color: Colors.grey.shade700)),
+                      child: IconButton(
+                        padding: const EdgeInsets.all(0),
+                        splashRadius: 15,
+                        onPressed: () =>
+                            _quantidade != 0 ? _subQuantidade() : {},
+                        icon: Icon(
+                          Icons.remove,
+                          color: Colors.grey.shade700,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                        child: const Text('Cancelar',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.purple,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                        child: const Text('Adicionar produto'),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            print("Dados: \nNome: " +
+                                _nameProduct.text +
+                                "\nTag: " +
+                                _tagProduct.text +
+                                "\nQuantidade: " +
+                                _quantProduct.text);
+                            setState(
+                              () {
+                                Navigator.of(context).pop();
+                              },
+                            );
+                          } else {
+                            _updateHeight();
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.purple,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
