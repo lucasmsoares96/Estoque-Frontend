9466e4f9f9bbe9b3c8147968b7354f34de4cb0dd
Gustavo Barcelos <gustavo05rb@gmail.com>
2021-12-28 09:04:08 -0300

    Ajustes finais na tela de login

---
+++ lib/pages/login/components/dialog_login.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+Future genericDialog(BuildContext context, String title, String body, String buttonName) async{
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      // retorna um objeto do tipo Dialog
+      return AlertDialog(
+        title: Text(title),
+        content: Text(body),
+        actions: <Widget>[
+          // define os botões na base do dialogo
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(const Color(0xFF7B2CBF)),
+            ),
+            child: Text(buttonName),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
